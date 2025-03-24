d85399c155db1732c6cc0d292493db4911a71794
Shyamkrishna B Nair <shyamkrishnabnair@gmail.com>
2025-03-25 01:55:22 +0530

    this da model hahahahahahhahaihafilhfsliuvhrwigrw

---
+++ model/model.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import ollama
+import base64
+
+st.set_page_config(page_title="Endxiety, your Mental Wellness Companion")
+
+st.session_state.setdefault('conversation_history',[])
+
+def generate_response(user_input):
+    st.session_state['conversation_history'].append({"role":"user", "content":user_input})
+
+    response = ollama.chat(model="gemma3:1b", messages=st.session_state['conversation_history'])
+    ai_response= response['message']['content']
+
+    st.session_state['conversation_history'].append({"role":"assistant", "content":ai_response})
+    return ai_response
+
+
+st.title("Endxiety, your Mental Wellness Companion")
+
+for msg in st.session_state['conversation_history']:
+    role= "You" if msg['role'] == "user" else "AI"
+    st.markdown(f"**{role}:** {msg['content']}")
+
+user_message = st.text_input("How can I help you today?")
+
+if user_message:
+    with st.spinner("Thinking....."):
+        ai_response = generate_response(user_message)
+        st.markdown(f"**AI:** {ai_response}")
+
+  
